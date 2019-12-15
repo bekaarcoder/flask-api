8a03ea302e38d69dfdeb0be7a0fab3d2a75d1e19
Shashank <s_shashank@outlook.com>
2019-12-15 18:40:30 +0530

    authentication and delete

--- run.py
+++ run.py
@@ -1,23 +1,41 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = "secret"
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': 'Not Found'}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.99}
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': F"An item with name {name} already exists."}, 400
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item Deleted'}
+
+class Items(Resource):
+    def get(self):
+        return {'items': items}
+
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(Items, '/items')
 
 app.run(port=5000)
