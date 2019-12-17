0ffb181377e650c0881ff91e82e6ef8e08d77b4c
Shashank <s_shashank@outlook.com>
2019-12-17 22:46:43 +0530

    add user to database

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
+from user import UserRegister
 
 app = Flask(__name__)
 app.secret_key = "secret"
@@ -54,5 +55,6 @@ class Items(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Items, '/items')
+api.add_resource(UserRegister, '/register')
 
 app.run(port=5000)
